30bbf662fe71682c296b90c1063780948dc072f7
AlanCoding <arominge@redhat.com>
2017-04-27 13:36:30 -0400

    script examples through reading from file

---
+++ scripts/read_file.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import json
+import os
+
+
+filename = os.environ['ENV_VAR']
+
+
+with open(filename, 'r') as f:
+    contents = f.read().strip()
+
+
+print json.dumps({
+    "_meta": {
+        "hostvars": {}
+    },
+    "all": {
+        "hosts": [contents]
+    },
+})
