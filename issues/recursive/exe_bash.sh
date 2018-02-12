99d5be217e044ec906c5a49fdbdc32482bb31096
AlanCoding <arominge@redhat.com>
2018-02-12 14:24:59 -0500

    add example of ansible-inventory target that calls ansible-inventory

---
+++ issues/recursive/exe_bash.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ansible-inventory -i $DIR/foobar.py --list
