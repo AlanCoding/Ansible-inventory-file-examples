1c8b54da3a3d9d02268427cab8ea83382c1b899f
AlanCoding <arominge@redhat.com>
2018-02-15 12:58:36 -0500

    script example of all basic variable types

---
+++ scripts/basic_vars/inv.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import json
+
+
+print json.dumps({
+    "_meta": {
+        "hostvars": {
+            "ahost": {"host_var": 4}
+        }
+    },
+    "all": {
+        "vars": {
+            "inventory_var": 1
+        }
+    },
+    "agroup": {
+        "hosts": ["foobar"],
+        "vars": {
+            "group_var": 2
+        }
+    }
+})
