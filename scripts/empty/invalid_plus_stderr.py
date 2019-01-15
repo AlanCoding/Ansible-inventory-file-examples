68d3107c9b7e71c185b2f0b926af0057d96cdc5a
AlanCoding <arominge@redhat.com>
2019-01-15 13:42:50 -0500

    add script that gives invalid output and writes stderr

---
+++ scripts/empty/invalid_plus_stderr.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+import sys
+
+
+# Write to standard error
+print('TEST', file=sys.stderr)
+
+print('{"_meta": {"hostvars": {}}')
